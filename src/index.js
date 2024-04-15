b767191ce5c4edd4acc9513e5e7fef65c8cd46b5
Mariano Masondo <nanomason@gmail.com>
2024-04-15 12:11:33 -0300

    Broken routes

--- src/index.js
+++ src/index.js
@@ -1,43 +1,15 @@
 const express = require("express");
-const { StatusCodes } = require ("http-status-codes");
+// const { StatusCodes } = require ("http-status-codes");
 
-const approutes = require("./routes")
+const appRoutes = require("./routes")
 
 const app = express();
 const port = 3000;
 
-const STATUS = {
-    SUCCESS: "OK",
-    FAIRLURE: "NO"
-}
 app.use(express.json())
 
-app.use("/v1, approutes")
-
-
-app.get("/hello-you", (req, res ) => {
-    res.status(StatusCodes.OK)
-    res.send("Hello you!");
-});
-
-app.post("/add", (req, res) => {
-    const data = [];
-    const { body } = req;
-
-    if(!body.name) {
-        return res.status(StatusCodes.BAD_REQUEST).send({
-            status: STATUS.FAIRLURE,
-            messagge: "Name is required"
-        });
-    }
-    return res.status(StatusCodes.CREATED).send({
-        status: STATUS.SUCCESS,
-        
-        messagge: data,
-    });
-})
-
+app.use("/v1", appRoutes)
 
 app.listen(port, () => {
-    console.log(`Server started on http://localhost: ${port}`);
+    console.log(`Server started on http://localhost:${port}`);
 })
