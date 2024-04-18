283f2512874e937ef558d8545145c84af0ab994b
Mariano Masondo <nanomason@gmail.com>
2024-04-18 18:22:04 -0300

    Code format

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 // const { StatusCodes } = require ("http-status-codes");
 
-const appRoutes = require("./routes")
+const appRoutes = require("./routes");
 
 const app = express();
 const port = 3000;
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/v1", appRoutes)
+app.use("/v1", appRoutes);
 
 app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`);
-})
+  console.log(`Server started on http://localhost:${port}`);
+});
--- src/routes.js
+++ src/routes.js
@@ -5,29 +5,29 @@ const router = express.Router();
 const port = 3000;
 
 const STATUS = {
-    status: "OK",
-    failure: "NO"
-}
+  status: "OK",
+  failure: "NO",
+};
 
 router.get("/hello-word", (req, res) => {
-    res.status(StatusCodes.OK);
-    res.send("Hello word!");
+  res.status(StatusCodes.OK);
+  res.send("Hello word!");
 });
 
 router.post("/add", (req, res) => {
-    const data = [];
-    const { body } = req;
+  const data = [];
+  const { body } = req;
 
-    if (!body.name) {
-        return res.status(StatusCodes.BAD_REQUEST).send({
-            status: STATUS.failure,
-            message: "Name is required"
-        });
-    }
-    return res.status(StatusCodes.CREATED).send({
-        status: STATUS.success,
-        message: data,
+  if (!body.name) {
+    return res.status(StatusCodes.BAD_REQUEST).send({
+      status: STATUS.failure,
+      message: "Name is required",
     });
+  }
+  return res.status(StatusCodes.CREATED).send({
+    status: STATUS.success,
+    message: data,
+  });
 });
 
 export default router;
